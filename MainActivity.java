4c38b1dc90b88692cd37e1ed5ef998a694155767
anirudhbansal <anirudhbansal95@gmail.com>
2016-01-01 13:17:13 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,16 +4,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-public class MainActivity extends Activity {
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+public class MainActivity extends Activity {
+EditText username,password;
+    TextView error;
+    String error_message="Username and Password don't match.Please try again.";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.main_user);
+        password=(EditText)findViewById(R.id.main_password);
+        error=(TextView)findViewById(R.id.error_login);
     }
 public void onMainReg(View view){
     Intent intent=new Intent(this,RegisterActivity.class);
     startActivity(intent);
 }
+public void onMainLog(View view){
+    String s1=username.getText().toString();
+    String s2=password.getText().toString();
+    try{
+        if(s1.length() > 0 && s2.length() >0)
+        {
+            UserDatabaseHelper dbUser = new UserDatabaseHelper(MainActivity.this);
+            dbUser.open();
+            if(dbUser.Login(s1,s2))
+            {
+                error.setText("");
+            }else{
+               error.setText(error_message);
+            }
+            dbUser.close();
+        }
 
+    }catch(Exception e)
+    {
+        Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_LONG).show();
+    }
+}
 }
